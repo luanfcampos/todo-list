5c0554eb50c59529171a02997c3e617f868e2611
Luan Ferreira Campos <86481756+luanfcampos@users.noreply.github.com>
2021-10-05 10:34:39 -0300

    estrutura do app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
+import {useState} from 'react';
 import * as C from './App.styles'
+import { Item } from './types/item'
+import { ListItem } from './components/ListItem'
 
 const App = () => {
+  const [list, setList] = useState<Item[]>([
+    {id: 1, name: 'Comprar o pão na padaria', done: false},
+    {id: 2, name: 'Comprar um bolo', done: false},
+  ])
+  
   return (
     <C.Container>
-        
+        <C.Area>
+          <C.Header>
+            Lista de Tarefas
+          </C.Header>
+
+          {/*Área de adicionar tarefas*/}
+
+          {list.map((item, index)=>(
+              <ListItem key={index} item={item}/>
+          ))}
+
+        </C.Area>
     </C.Container>
   );
 };
